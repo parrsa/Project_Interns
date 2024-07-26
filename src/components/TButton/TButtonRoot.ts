de45e8690b06ac4a5142c74b6b3dcc4558a21fb6
pegah-la <pegahlamie@gmail.com>
2024-07-26 12:10:00 +0330

    add Remove function in TButtonRoot

--- src/components/TButton/TButtonRoot.ts
+++ src/components/TButton/TButtonRoot.ts
@@ -1,40 +1,58 @@
 import { Button, styled } from "@mui/material";
 import { TButtonOptions } from "../../@types/components/MUI/TButton";
-const TButtonRoot = styled(Button)<{ ownerState: TButtonOptions }>(({ ownerState }) => {
-    const { variant, height, add, submit } = ownerState;
+
+const TButtonRoot = styled(Button)<{ ownerState: TButtonOptions }>(
+  ({ ownerState }) => {
+    const { variant, height, add, submit,size,remove } = ownerState;
 
     function Add() {
-        return (
-            {
-                width: "8rem",
-                height: "10rem",
-                backgroundColor: 'red'
-            }
-        )
-    };
+      return {
+        width: "8rem",
+        height: "10rem",
+        backgroundColor: "red",
+      };
+    }
 
     function Submit() {
-        return variant === "contained" ? {
-            fontSize: '13px',
-            color: 'red',
-            backgroundColor: 'blue',
-            border: '2px solid green',
-            width: '150px',
-            height: '150px'
-        } : {
-            fontSize: '13px',
-            color: 'red',
-            backgroundColor: 'yellow',
-            width: '100px',
-            height: '100px'
-        }
+      return variant === "contained"
+        ? {
+            fontSize: "13px",
+            color: "red",
+            backgroundColor: "blue",
+            border: "2px solid green",
+            width: "150px",
+            height: "150px",
+          }
+        : {
+            fontSize: "13px",
+            color: "red",
+            backgroundColor: "yellow",
+            width: "100px",
+            height: "100px",
+          };
     }
-
-
+    function Remove(){
+      return size === 'medium'?
+      {
+        fonSize:'1.3rem',
+        fontWeight : '600',
+        color:'#dedede',
+        backgroundColor:'#555'
+      } :
+      {
+        fonSize:'1rem',
+        fontWeight : '500',
+        color:'#555',
+        backgroundColor:'#dedede'
+      }
+    }
 
     return {
-        ...(add && Add()),
-        ...(submit && Submit())
-    }
-})
-export default TButtonRoot
+      ...(add && Add()),
+      ...(submit && Submit()),
+      ...(remove && Remove())
+     
+    };
+  }
+);
+export default TButtonRoot;
