5dcba0b27cb8eeddf08ef6b858165fea3b3141c3
pegah-la <pegahlamie@gmail.com>
2024-07-26 12:20:16 +0330

    change variant to optional and add remove type

--- src/@types/components/MUI/TButton/index.ts
+++ src/@types/components/MUI/TButton/index.ts
@@ -6,11 +6,12 @@ export interface TButtonOptions {
     width?: CSS.Property.Width | number;
     height?: CSS.Property.Height | number;
     sx?: SxProps;
-    variant: 'text' | 'contained' | "outlined" | "gradient";
+    variant?: 'text' | 'contained' | "outlined" | "gradient";
     size?: "small" | "medium" | "large";
     children?: React.ReactNode,
-    add?: boolean | string
-    submit?: boolean | string
+    add?: boolean | string,
+    submit?: boolean | string,
+    remove ?: boolean |string
 }
 
 export type ITButton = ButtonProps & TButtonOptions;
